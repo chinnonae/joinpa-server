13a651b6e08c738445ea2101e5672f07ed4958b3
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-06-01 07:51:48 +0700

    check for joined or invited user in /event/invite

--- Express-route/event-route.js
+++ Express-route/event-route.js
@@ -108,6 +108,11 @@ module.exports.assignRoute = function(app){
       /*
         add this user friend to pendingList and save.
       */
+      if(event.joinedList.indexOf(invitation.friendId) > 0 || event.pendingList.indexOf(invitation.friendId) > 0){
+        return res.status(200).json({
+          message: 'Your friend has been invited or joined already.'
+        })
+      }
       event.pendingList.push(invitation.friendId);
       event.save(function(err) {
 
